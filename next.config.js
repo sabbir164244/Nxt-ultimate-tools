8aa0f73ee58324764517291842a0c211a5b226e3
sabbir164244 <smd164244@gmail.com>
2025-07-20 05:07:54 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,23 +7,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // --- YEH HAI AAKHRI RAKSHAS KO HARANE KA JAADU ---
-  webpack: (config, { isServer }) => {
-    // Rule for 'import.meta'
-    config.module.rules.push({
-      test: /\.m?js$/,
-      type: "javascript/auto",
-      resolve: {
-        fullySpecified: false,
-      },
-    });
-
-    // Rule to IGNORE the problematic server-side package
-    config.externals = [...config.externals, 'onnxruntime-node'];
-    
-    return config;
-  },
-  // --- JAADU KHATAM ---
 };
 
 module.exports = nextConfig;
